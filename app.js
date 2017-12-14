1b3bef73ff03bab6efc11eb3dfddcc1792eaf75b
Troels Ugilt Jensen <tujensen@gmail.com>
2017-12-14 11:28:13 +0100

    MKBDIGI-223: Fixed code styles. Added better get output

--- app.js
+++ app.js
@@ -4,47 +4,59 @@ const mariadb = require('mariasql');
 const config = require('./config');
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());         // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
 }));
 
 app.get('/', (req, res) => {
-  let c = new mariadb(config.mariadb);
+    let c = new mariadb(config.mariadb);
 
-  c.query('SELECT * FROM sensor',
-    function(err, rows) {
-      if (err)
-        throw err;
+    c.query('SELECT * FROM sensor ORDER BY id DESC limit 50',
+        function (err, rows) {
+            if (err) {
+                throw err;
+            }
 
-      console.dir(rows);
-    });
+            let result = '<style>td { border: 1px solid black; margin: 0}</style><h1>Sensor data</h1><table>';
 
+            for (let row of rows) {
+                result += '<tr>';
+                result += '<td>' + row.id + '</td>';
+                result += '<td>' + row.sensor + '</td>';
+                result += '<td>' + row.sequence + '</td>';
+                result += '<td>' + row.timestamp + '</td>';
+                result += '<td>' + row.data + '</td>';
+                result += '</tr>';
+            }
 
-  c.end();
+            res.send(result);
+        });
 
-  res.send('Hello World!')
+    c.end();
 });
 
 app.post('/', (req, res) => {
-  let c = new mariadb(config.mariadb);
-  let insertQuery = c.prepare('INSERT INTO sensor (sensor, sequence, data) VALUES (:sensor, :sequence, :data)');
+    let c = new mariadb(config.mariadb);
+    let insertQuery = c.prepare('INSERT INTO sensor (sensor, sequence, data) VALUES (:sensor, :sequence, :data)');
 
-  let body = req.body;
+    let body = req.body;
 
-  console.log("body", body);
+    if (body.hasOwnProperty('EUI') && body.hasOwnProperty('data') && body.hasOwnProperty('seqno')) {
+        c.query(insertQuery({
+            sensor: body.EUI,
+            sequence: body.seqno,
+            data: body.data
+        }), function (err) {
+            if (err) {
+                throw err;
+            }
+        });
+    }
 
-  if (body.hasOwnProperty('EUI') && body.hasOwnProperty('data') && body.hasOwnProperty('seqno')) {
-    c.query(insertQuery({ sensor: body.EUI, sequence: body.seqno, data: body.data }), function(err, rows) {
-      if (err)
-        throw err;
-      console.dir(rows);
-    });
-  }
+    c.end();
 
-  c.end();
-
-  res.send('')
+    res.send('');
 });
 
-app.listen(3000, () => console.log('Sensor listening on port 3000!'));
+app.listen(3000);
--- setup_db.js
+++ setup_db.js
@@ -2,28 +2,24 @@ const mariadb = require('mariasql');
 const config = require('./config');
 
 let db = {
-  host: config.mariadb.host,
-  user: config.mariadb.user,
-  password: config.mariadb.password,
+    host: config.mariadb.host,
+    user: config.mariadb.user,
+    password: config.mariadb.password,
 };
 
 let c = new mariadb(db);
 
-c.query('DROP DATABASE IF EXISTS ' + config.mariadb.db + ';');
+c.query('CREATE DATABASE IF NOT EXISTS ' + config.mariadb.db + ';', function () {
 
-c.query('CREATE DATABASE IF NOT EXISTS ' + config.mariadb.db + ';', function (err) {
-
-  console.log("Creating table: " + config.mariadb.db + ".sensor if it does not exist.");
-
-  c.query('CREATE TABLE IF NOT EXISTS ' + config.mariadb.db + '.sensor ' +
+    c.query('CREATE TABLE IF NOT EXISTS ' + config.mariadb.db + '.sensor ' +
     '(`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, ' +
     '`sensor` VARCHAR(32), ' +
     '`sequence` INT, ' +
-    '`timestamp` TIMESTAMP, `data` BLOB);', function (err, rows) {
-    if (err) {
-      throw err;
-    }
-  });
+    '`timestamp` TIMESTAMP, `data` BLOB);', function (err) {
+        if (err) {
+            throw err;
+        }
+    });
 });
 
 c.end();
