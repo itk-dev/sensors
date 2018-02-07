4c8a267bb1b942da74a19754b40de08665dde506
Troels Ugilt Jensen <tujensen@gmail.com>
2018-02-07 09:48:13 +0100

    CIT-33: Fixed first version of feed

--- plugins/sensordata/sensordata.js
+++ plugins/sensordata/sensordata.js
@@ -4,44 +4,144 @@
  */
 'use strict';
 
-const config = require('./../../config');
 const Influx = require('influx');
 
 module.exports = function setup (options, imports, register) {
-  const eventBus = imports.eventbus;
-  const server = imports.server;
-  const logger = imports.logger;
-  const influxdb = imports.influxdb;
+    const server = imports.server;
+    const logger = imports.logger;
+    const influxdb = imports.influxdb;
 
-  /**
-   * GET: /api/sensordata/citylab
-   */
-  server.get('/api/sensordata/citylab', (req, res) => {
-    let queries = [];
+    /**
+     * GET: /api/sensordata/citylab
+     */
+    server.get('/api/sensordata/citylab', (req, res) => {
+        let queries = [];
 
-    for (let sensorParameter of req.query.sensor) {
-      let sensorPair = sensorParameter.split(',');
+        let queryParameters = req.query.sensor;
 
-      let type = Influx.escape.measurement(sensorPair[1]);
-      let sensor = Influx.escape.tag(sensorPair[0]);
+        const location = 'citylab';
 
-      let query = `select value from "${type}" where sensor = "${sensor}" order by time desc limit 1`;
+        const types = {
+            battery: {
+                title: 'Batteri',
+                fa_icon: 'battery-three-quarters',
+                unit: '%'
+            },
+            charging_power: {
+                title: 'Ladestrøm',
+                fa_icon: 'bolt',
+                unit: 'mA'
+            },
+            air_temperature: {
+                title: 'Lufttemperatur',
+                fa_icon: 'thermometer-half',
+                unit: '°C'
+            },
+            water_temperature: {
+                title: 'Vandtemperatur',
+                fa_icon: 'thermometer-half',
+                unit: '°C'
+            },
+            humidity: {
+                title: 'Luftfugtighed',
+                fa_icon: 'tint',
+                unit: '%'
+            },
+            pressure: {
+                title: 'Lufttryk',
+                fa_icon: 'tachometer-alt',
+                conversion: function (val) {
+                    return val * .01;
+                },
+                unit: 'mBar'
+            },
+            distance_to_water: {
+                title: 'Vandstand',
+                fa_icon: 'arrows-v',
+                unit: 'cm'
+            },
+            wind_vane: {
+                title: 'Vindretning',
+                conversion: function (val) {
+                    let values = ['W','WSW','SW','SSW','S','SSE','SE','ESE','E','ENE','NE','NNE','N','NNW','NW','WNW'];
+                    return values[val];
+                },
+                fa_icon: 'compass'
+            },
+            wind_speed: {
+                title: 'Vindhastighed',
+                unit: 'km/h',
+                fa_icon: 'pennant'
+            },
+            rain: {
+                title: 'Regn',
+                unit: 'mm/h',
+                fa_icon: 'tint'
+            },
+            solar_radiation: {
+                title: 'Solstråling',
+                // @TODO
+                unit: '',
+                fa_icon: 'sun'
+            },
+            lux: {
+                title: 'Dagslys',
+                unit: 'Lux',
+                fa_icon: 'sun'
+            }
+        };
 
-      queries.push(query);
-    }
+        if (!Array.isArray(queryParameters)) {
+            queryParameters = [queryParameters];
+        }
 
-    influxdb.query(queries).then(result => {
+        let requests = [];
 
+        for (let sensorParameter of queryParameters) {
+            let sensorPair = sensorParameter.split(',');
 
-      res.json(result);
-    }).catch(err => {
-      console.log(err);
-      res.status(500).send(err.stack);
+            requests.push(sensorPair);
+
+            let type = Influx.escape.measurement(sensorPair[1]);
+            let sensor = Influx.escape.tag(sensorPair[0]);
+
+            let query = `select * from "${type}" where "sensor" = '${sensor}' order by time desc limit 1`;
+
+            queries.push(query);
+        }
+
+        influxdb.query(queries).then(influxResults => {
+            let response = [];
+
+            for (let i = 0; i < influxResults.length; i++) {
+                for (let j = 0; j < influxResults[i].length; j++) {
+                    let result = influxResults[i][j];
+                    let request = requests[i];
+                    let type = types[request[1]];
+
+                    let res = {
+                        sensor: request[0],
+                        name: type.title,
+                        unit: type.unit,
+                        location: location,
+                        fa_icon: type.fa_icon,
+                        timestamp: result.time.toISOString(),
+                        value: type.hasOwnProperty('conversion') ? type.conversion(result.value) : result.value
+                    };
+
+                    response.push(res);
+                }
+            }
+
+            res.json(response).send();
+        }).catch(err => {
+            logger.error(err);
+            res.status(500).send();
+        });
     });
-  });
 
-  /**
-   * Register with architect.
-   */
-  register(null, {});
+    /**
+     * Register with architect.
+     */
+    register(null, {});
 };
