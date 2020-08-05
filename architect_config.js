988927871e9ff76914282828d4485a820d8de5bc
Lars Steen <lats@aarhus.dk>
2020-08-05 13:23:26 +0200

    SALG-865: Adding elsys sensor parser

--- architect_config.js
+++ architect_config.js
@@ -5,6 +5,7 @@ module.exports = [
     './plugins/server',
     './plugins/api',
     './plugins/influxdb',
+    './plugins/elsys_sensor_parser',
     './plugins/parser0004A30B001E8EA2',
     './plugins/parser0004A30B001E307C',
     './plugins/parser0004A30B001E1694',
